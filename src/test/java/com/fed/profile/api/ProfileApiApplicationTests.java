3f54b13e47e1d424b9570278b470cba10b58e4f3
Eren Dalçık <dalcikferhateren@gmail.com>
2024-01-11 14:27:26 +0300

    spring files

---
+++ src/test/java/com/fed/profile/api/ProfileApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fed.profile.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
